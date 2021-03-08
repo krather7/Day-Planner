0d90fbd607e17325f302b39b7032f59ce10ea766
krather7 <75811296+krather7@users.noreply.github.com>
2021-03-08 10:52:31 -0600

    Fixed hourly blocks to display correct colors

--- script.js
+++ script.js
@@ -30,10 +30,23 @@ $(document).ready(function () {
   //Runs function created above
   currentTime()
   //=========================================================
-  //Event listener that stores new values into local storage
+  //Event listener that stores new values into local storage as a string
   button.on('click', function (event) {
         event.preventDefault();
     var userText = $(this).siblings('.col-sm-10').val().replace(/['"]+/g, '');
     var storeThis = $(this).parent().attr('id');
         localStorage.setItem(storeThis, JSON.stringify(userText));
   })
+  //=========================================================
+  //Prints user's text and removes default empty quotations.
+  $('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
+  $('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
+  $('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
+  $('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
+  $('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
+  $('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
+  $('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
+  $('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
+  $('#17 textarea').val(localStorage.getItem('17').replace(/['"]+/g, ''));
+});
+  //=========================================================
