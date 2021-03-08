1c7c7dca7fedecd5fff3f86379b129a5f4951201
krather7 <75811296+krather7@users.noreply.github.com>
2021-03-08 10:27:03 -0600

    Finished local time setting through Moment

--- script.js
+++ script.js
@@ -37,16 +37,3 @@ $(document).ready(function () {
     var storeThis = $(this).parent().attr('id');
         localStorage.setItem(storeThis, JSON.stringify(userText));
   })
-  //=========================================================
-  //Prints user's text and removes default empty quotations.
-  $('#9 textarea').val(localStorage.getItem('9').replace(/['"]+/g, ''));
-  $('#10 textarea').val(localStorage.getItem('10').replace(/['"]+/g, ''));
-  $('#11 textarea').val(localStorage.getItem('11').replace(/['"]+/g, ''));
-  $('#12 textarea').val(localStorage.getItem('12').replace(/['"]+/g, ''));
-  $('#13 textarea').val(localStorage.getItem('13').replace(/['"]+/g, ''));
-  $('#14 textarea').val(localStorage.getItem('14').replace(/['"]+/g, ''));
-  $('#15 textarea').val(localStorage.getItem('15').replace(/['"]+/g, ''));
-  $('#16 textarea').val(localStorage.getItem('16').replace(/['"]+/g, ''));
-  $('#17 textarea').val(localStorage.getItem('17').replace(/['"]+/g, ''));
-});
-  //=========================================================
